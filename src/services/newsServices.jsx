13f46b1e529e9cb51fb01759d4a236fc0a13e8cf
aryariski <aryariski9a@gmail.com>
2026-04-20 20:14:50 +0700

    switch fetch api from local to prod

--- src/services/newsServices.jsx
+++ src/services/newsServices.jsx
@@ -1,5 +1,8 @@
 export default async function getNews() {
+    /* Local development
     const res = await fetch('/api/cnn-news')
+    */
+    const res = await fetch('https://berita-indo-api-next.vercel.app/api/cnn-news')
     const data = await res.json()
     return data.data
 }
