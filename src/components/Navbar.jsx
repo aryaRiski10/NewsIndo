a06b3f71bd32cc8ff1f5b7ee9317a4180c7a25c3
aryariski <aryariski9a@gmail.com>
2026-04-22 14:05:29 +0700

    update: active menu/source when visit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Navbar({ sources }) {
+    const [activeSource, setActiveSource] = useState('')
     return (
         <div className="navbar container">
             <nav className="nav nav-news flex flex-wrap items-center justify-center gap-[16px]">
-                <Link to="/" className="nav-link nav-news-item">Home</Link>
+                <Link to="/" className={`nav-link nav-news-item ${activeSource === '' ? 'active' : ''}`} onClick={() => setActiveSource('')}>Home</Link>
                 {sources.map((item, index) => (
-                    <Link key={index} to={`/source/${item.toLowerCase().replace(/ /g, '-')}`} className="nav-link nav-news-item">{item}</Link>
+                    <Link key={index} to={`/source/${item.toLowerCase().replace(/ /g, '-')}`} className={`nav-link nav-news-item ${activeSource === item ? 'active' : ''}`} onClick={() => setActiveSource(item)}>{item}</Link>
                 ))}
             </nav>
         </div>
